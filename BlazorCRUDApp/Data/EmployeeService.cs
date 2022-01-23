b6edf0acf061b7289f897d048f1bc810a9cc4da3
Theminda Srimal <67902627+ThemindaSrimal@users.noreply.github.com>
2022-01-23 13:41:54 +0530

    Add files via upload

---
+++ BlazorCRUDApp/Data/EmployeeService.cs
@@ -0,0 +1,49 @@
+﻿namespace BlazorCRUDApp.Data
+{
+    public class EmployeeService : IEmployeeService
+    {
+
+        private List<Employee> employees = new List<Employee>
+        {
+            new Employee{
+                Id = Guid.NewGuid(),
+                Name = "Employee 1"
+            },
+            new Employee{
+                Id = Guid.NewGuid(),
+                Name = "Employee 2"
+            },
+
+        };
+
+        public void AddEmployee(Employee employee)
+        {
+            var id = Guid.NewGuid();
+            employee.Id = id;
+            employees.Add(employee);
+        }
+
+        public void DeleteEmployee(Guid id)
+        {
+            var employee = GetEmployee(id);
+            employees.Remove(employee);
+        }
+
+        public Employee GetEmployee(Guid id)
+        {
+            return employees.SingleOrDefault(x => x.Id == id);
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            var getOldEmployee = GetEmployee(employee.Id);
+            getOldEmployee.Name = employee.Name;
+ 
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+    }
+}
---
+++ BlazorCRUDApp/Data/IEmployeeService.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorCRUDApp.Data
+{
+    public interface IEmployeeService
+    {
+
+        List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        void UpdateEmployee (Employee employee);
+
+        void AddEmployee (Employee employee);
+
+        void DeleteEmployee (Guid id);
+    }
+}
